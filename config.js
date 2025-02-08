196011f61faa3fecad52b3ae38936e425a5b2089
Monumodmod <163610394+Monumodmod@users.noreply.github.com>
2025-02-08 11:39:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -5,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~CiRwEbiZ#Xpji9czDcBcCk1xoZL0y-wa-YiMxoCkLjnhRw_JSrfU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
